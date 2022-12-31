1c0ea12c5d2260c9d33f4637bdc75c8d6e76006f
ajayengineer <ajaywebsolution@gmail.com>
2022-12-30 18:44:58 -0800

    brand section added in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,31 @@ const Header = () => {
     return (
         <div className='container-fluid bg-dark'>
             <div className='container'>
-                <nav>
-                    <ul className='myNavi'>
-                        <NavLink to="/">
-                            <li>Home </li>
-                        </NavLink>
-                        <NavLink to="/productlisting">
-                            <li>Products</li>
-                        </NavLink>
-                        <NavLink to="/register">
-                            <li>User Register</li>
-                        </NavLink>
-                        <NavLink to="/adminregister">
-                            <li>Register as Admin</li>
-                        </NavLink>
-                    </ul>
-                </nav>
+                <div className='row'>
+                    <div className='col-md-3'>
+                        <NavLink to="/"><h1>Bank App</h1></NavLink>
+                    </div>
+                    <div className='col-md-9'>
+                        <nav>
+                            <ul className='myNavi'>
+                                <NavLink to="/">
+                                    <li>Home </li>
+                                </NavLink>
+                                <NavLink to="/productlisting">
+                                    <li>Products</li>
+                                </NavLink>
+                                <NavLink to="/register">
+                                    <li>User Register</li>
+                                </NavLink>
+                                <NavLink to="/adminregister">
+                                    <li>Register as Admin</li>
+                                </NavLink>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     )
